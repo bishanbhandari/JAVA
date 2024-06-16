5871a5fa0b133de1ade9fb5c5edc59aa512ca25f
bishanbhandari <108720684+bishanbhandari@users.noreply.github.com>
2024-06-16 10:23:44 +0545

    Add files via upload

---
+++ java/Enemy.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.io.File;
+import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+public class Enemy extends JPanel {
+    Random randomnumber=new Random();
+    int x=randomnumber.nextInt(500);
+    int y=randomnumber.nextInt(200);
+    Image image;
+Enemy(){
+    try {
+        image=ImageIO.read(new File("./image.png"));
+    } catch( Exception e) {
+        System.out.println("handle");
+    }
+}
+     public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.white);
+        g.drawImage(image,x, y, 50, 50,this);
+    }
+    public void update(){
+        y+=1;
+    }
+}
